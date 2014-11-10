338b141b0b67cf2f81bad7dfe6a584bfcf814fe9
Churak <jfr7294@gmail.com>
2014-11-09 21:25:49 -0500

    worked on getting the data parsing right. Fuck python cause it's a piece of shit.

--- src/motor_node/scripts/listener.py
+++ src/motor_node/scripts/listener.py
@@ -10,18 +10,16 @@ import math
 import binascii
 import serial
 import struct
+import string
 import crc16
 from std_msgs.msg import Float32
 
-
-motor_max = 2000.0
+motor_max = 4000.0
 count_rev = 16384.0
 
 gain_access = "\xA5\x3F\x02\x07\x00\x01\xB3\xE7\x0F\x00\x10\x3E"
 enable_bridge = "\xA5\x3F\x02\x01\x00\x01\x01\x47\x00\x00\x00\x00"
-#gofast = "\xA5\x3F\x02\x45\x00\x02\xF0\x49\x55\x55\x01\x00\x4F\x71"
 vel_mode = "\xA5\x3F\x02\x45\x00\x02"
-#set up serial port
 
 motor_com_1 = serial.Serial(
 	port='/dev/ttyS0',
@@ -40,33 +38,28 @@ motor_com_2 = serial.Serial(
 )
 
 #close and re-open for shits and gigs
-
 #motor_com_1.close()
 #motor_com_1.open()
+
 if motor_com_1.isOpen()==True:
 	
 	print 'COM0 is open!'
 
 	#Gain write-access to motor controllers
-
 	motor_com_1.write(gain_access)
 
-#	print 'Write access granted!'
-
 	#Enable Bridge
-
 	motor_com_1.write(enable_bridge)
 
-	print 'Bridge Enabled!'
-
-#	motor_com_1.write(gofast)
 
 if motor_com_2.isOpen()==True:
 
 	print 'COM1 is open!'
 
+	#Gain write-access to motor controllers
 	motor_com_2.write(gain_access)
-	
+
+	#Gain write-access to motor controllers
 	motor_com_2.write(enable_bridge)
 
 def motormath(vscale):
@@ -79,66 +72,38 @@ def motormath(vscale):
 
 def checksum_calc(velocity):
 
-	cksum = crc16.crc16xmodem(str(velocity))
-	print "checksum stuck"
+	cksum = crc16.crc16xmodem(velocity)
 	return(cksum)
 
 def callbackmotor1(data):
 
 	vel_1 = motormath(data.data)
 
-	# Transform vel 1 to little endien respresentation
-	vel_1_endien = vel_1 
+	if vel_1 < 0: 
+		vel_1_hex = hex((abs(vel_1) ^ 0xffffffff) +1)[2:].strip( 'L' )
+	else:
+		vel_1_hex = hex(vel_1)[2:].zfill(8)
 
-	data_wordcount = math.floor(len(str(vel_1_endien))/2) #fill in here
+	vel_1_hex_int = int(vel_1_hex,16)
 
-	# This is where the velcity data will be parsed. The number of words will be parsed out of it
-	# 	#bytes = odd -> l = (#bytes - 1)/2
-	#	#bytes = even -> l = #bytes/2
+	# Represents te number in little endian notation
+	vel_1_final = struct.pack('<q', vel_1_hex_int).encode('hex')[:8]
+	vel_1_final_hex = "\\x" +  "\\x".join(vel_1_final[i:i+2] for i in range(0, 8, 2))
+
+        chk_sum_data = crc16.crc16xmodem(repr(vel_1_final))
+ 
+	#apples = ":".join("{:02x}".format(ord(c)) for c in str(motor_1_final))
 	
-	command_header = vel_mode + str(data_wordcount)
-
-	apples = ":".join("{:02x}".format(ord(c)) for c in command_header)
+	print vel_1_final
+	print hex(chk_sum_data)
 	
-	chk_sum_header = checksum_calc(command_header)
-	chk_sum_data = checksum_calc(vel_1_endien)
-
-	motor_1_final = command_header + str(chk_sum_header) + str(vel_1_endien) + str(chk_sum_data)
-
-	#motor_com_1.write(motor_1_final)
-
-	#print "pickles!"
-	
-	apples = ":".join("{:02x}".format(ord(c)) for c in str(chk_sum_data))
-
-	print apples
-
 	
 def callbackmotor2(data):
    
 	vel_2 = motormath(data.data)
 
-	# Transform vel 1 to little endien respresentation
-	
-	#vel_2_endien = struct.pack('<i', hex(vel_2))
 	vel_2_endien = vel_2
-
-	# This is where the velcity data will be parsed. The number of words will be parsed out of it
-	# 	#bytes = odd -> l = (#bytes - 1)/2
-	#	#bytes = even -> l = #bytes/2
 	
-	data_wordcount = math.floor(len(str(vel_2_endien))/2) #fill in here
-	#command_header = vel_mode + data_wordcount
-
-	#chk_sum_header = checksum_calc(command_header)
-	#chk_sum_data = checksum_calc(vel_2_endien)
-
-	#motor_2_final = command_header + str(chk_sum_header) + str(vel_2_endien) + str(chk_sum_data)
-
-	#motor_com_2.write(motor_2_final)
-
-	#print data_wordcount
-
 def motor():
 
 	sub1 = rospy.Subscriber("vel_1_float", Float32, callbackmotor1)
@@ -148,7 +113,7 @@ def motor():
 if __name__ == '__main__':
 
 	rospy.init_node('motor_control', anonymous=True)
-	#r = rospy.Rate(10)
+
 	motor()
 
 	try:
@@ -159,3 +124,5 @@ if __name__ == '__main__':
 			#r.sleep()
 
 	except rospy.ROSInterruptException: pass
+
+#!/usr/bin/env python
